8770c39308a908881b392e1f069d3e6db047a430
thomas.eikhaugen@gmail.com <thomas.eikhaugen@gmail.com>
2024-10-15 18:58:08 +0200

    feat: added eslint, husky and lint-staged

---
+++ eslint.config.mjs
@@ -0,0 +1,57 @@
+// eslint.config.js
+
+import js from '@eslint/js';
+import globals from 'globals';
+import eslintPluginJest from 'eslint-plugin-jest';
+
+export default [
+  // Base configuration for all JavaScript files
+  {
+    files: ['**/*.js'],
+    ignores: ['tests/**'], 
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        ...globals.browser, 
+        ...globals.es2021, 
+      },
+    },
+    plugins: {
+      jest: eslintPluginJest,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      eqeqeq: 'error',
+      curly: 'error',
+      'no-console': 'warn',
+      'no-unused-vars': 'warn',
+      'no-var': 'error',
+      'prefer-const': 'error',
+      'no-const-assign': 'error',
+      'no-debugger': 'warn',
+      'no-lonely-if': 'error',
+      'no-nested-ternary': 'error',
+      'no-unreachable': 'error',
+      'spaced-comment': ['error', 'always'],
+      'no-inline-comments': 'warn',
+    },
+  },
+  
+  {
+    files: ['**/*.test.js'],
+    ignores: [],
+    languageOptions: {
+      globals: {
+        ...globals.jest, 
+      },
+    },
+    plugins: {
+      jest: eslintPluginJest,
+    },
+    rules: {
+      ...eslintPluginJest.configs.recommended.rules,
+      'jest/prefer-expect-assertions': 'off',
+    },
+  },
+];
